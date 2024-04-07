a23a87f3e65e5e69160321de58ff0d989334b436
Tom Santens <tom.santens@gmail.com>
2024-04-07 14:39:58 +0200

    404 not found

--- src/Pages/Home.tsx
+++ src/Pages/Home.tsx
@@ -3,12 +3,13 @@ import logo from '../Edgeberry_logo_text.png';
 function Home() {
   return (
     <div className="App">
-      <header className="App-header">
+      <div className="App-centered">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        <br/>
+        <p style={{fontSize:'calc(10px + 2vmin)'}}>
           Kickstart your connected device adventure
         </p>
-      </header>
+      </div>
     </div>
   );
 }
