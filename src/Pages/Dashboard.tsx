cd4e94e81f59aed9dd033e892f8fcf3b75f82913
Tom Santens <tom.santens@gmail.com>
2024-04-10 11:50:53 +0200

    Backend concept + notifications

--- src/Pages/Dashboard.tsx
+++ src/Pages/Dashboard.tsx
@@ -1,7 +1,11 @@
-import { useEffect } from "react";
+import { useEffect, useState } from "react";
 import { api_things_getThingsList } from "../api/things";
+import NotificationBox from "../components/Notification";
 
 function Dashboard() {
+    const[ message, setMessage ] = useState<string>('');
+    const[ isError, setIsError ] = useState<boolean>(false);
+    const[ thingList, setThingList ] = useState<any[]>([]);
 
     useEffect(()=>{
         getThingsList();
@@ -10,11 +14,20 @@ function Dashboard() {
     async function getThingsList(){
         const result = await api_things_getThingsList();
         console.log(result);
+        if( result.message ){
+            setIsError(true);
+            setMessage(result.message);
+            return;
+        }
+        setThingList(result);
     }
 
     return (
         <div className="App">
             <h1>Dashboard</h1>
+            {thingList.map( (thing:any)=>{return <>{thing.thingName}<br/></>})}
+
+            <NotificationBox message={message} isError={isError}/>
         </div>
     );
 }
