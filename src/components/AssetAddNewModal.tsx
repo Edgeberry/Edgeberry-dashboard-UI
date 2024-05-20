2f2944c9dfb8360299dbe3abdc6b24fe2030e62f
Tom Santens <tom.santens@gmail.com>
2024-05-20 11:17:51 +0200

    Assets: Add asset steps concept

--- src/components/AssetAddNewModal.tsx
+++ src/components/AssetAddNewModal.tsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Alert, Button, Form, FormGroup, Modal } from "react-bootstrap";
+import { Alert, Button, Carousel, Form, FormGroup, Modal } from "react-bootstrap";
 import { faInfoCircle, faPlus } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
@@ -14,19 +14,44 @@ const AssetAddNewModal = ( props:{ show:boolean, onClose:Function })=>{
                     <Modal.Title><FontAwesomeIcon icon={faPlus} /> Add Device</Modal.Title>
                 </Modal.Header>
                 <Modal.Body>
-                    <Form>
-                        <Alert>
-                            <FontAwesomeIcon icon={faInfoCircle} /> Make sure your device is connected to the internet,
-                            and you have physical access to the device.
-                        </Alert>
-                        <FormGroup className="mb-2">
-                            <Form.Label>Enter the device Unique ID</Form.Label>
-                            <Form.Control type="text" placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"></Form.Control>
-                        </FormGroup>
+                <Carousel>
+                    <Carousel.Item>
+                        <Form>
+                            <Alert>
+                                <FontAwesomeIcon icon={faInfoCircle} /> Make sure your device is connected to the internet,
+                                and you have physical access to the device.
+                            </Alert>
+                            <FormGroup className="mb-2">
+                                <Form.Label>Enter the device Unique ID</Form.Label>
+                                <Form.Control type="text" placeholder="xxxxxxxx-xxxx-xxxx-xxxx-xxxxxxxxxxxx"></Form.Control>
+                            </FormGroup>
+                            <div style={{width:'100%', textAlign:'right'}}>
+                                <Button variant={'danger'}>Next</Button>
+                            </div>
+                        </Form>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Alert>Checking your device</Alert>
+                        <p>TODO: check whether the device is connected to the Edgeberry Cloud, and whether it is available to be assigned.</p>
+                    </Carousel.Item>    
+                    <Carousel.Item>
+                        <Alert>Press the button on the device</Alert>
+                        <p>TODO Maybe put some image here of the location of the button</p>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Alert>Agree to the terms</Alert>
+                        <p>After the user clicks agree, assign this device to this user</p>
                         <div style={{width:'100%', textAlign:'right'}}>
-                            <Button variant={'danger'}>Next</Button>
+                            <Button variant={'danger'}>Agree</Button>
                         </div>
-                    </Form>
+                    </Carousel.Item>
+                    <Carousel.Item>
+                        <Alert>Done</Alert>
+                        <p>Assign this device to this user</p>
+                        <p>Give a happy message for a second, and close the modal automatically</p>
+                    </Carousel.Item>
+                    </Carousel>
+
                 </Modal.Body>
             </Modal>
         </>);
