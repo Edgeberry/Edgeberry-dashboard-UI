03895de5913af65a15020e33cd52a4c5f0a16244
Tom Santens <tom.santens@gmail.com>
2024-06-12 10:24:02 +0200

    Admin: tabs for users and devices

--- src/Pages/Admin.tsx
+++ src/Pages/Admin.tsx
@@ -1,6 +1,6 @@
-import { Container } from 'react-bootstrap';
+import { Alert, Container, Tab, Tabs } from 'react-bootstrap';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faUnlockKeyhole } from '@fortawesome/free-solid-svg-icons';
+import { faInfoCircle, faMicrochip, faUnlockKeyhole, faUser } from '@fortawesome/free-solid-svg-icons';
 import { Navigate } from 'react-router-dom';
 
 function Admin(props:{user:any}) {
@@ -10,12 +10,21 @@ function Admin(props:{user:any}) {
   if(!props.user || !props.user?.roles.filter((role:string)=>{role.includes("admin")})){
     return <Navigate to="/dashboard"/>
   }
-  
+
   return (
-      <Container>
-            <br/>
-            <h2><FontAwesomeIcon icon={faUnlockKeyhole}/> Admin tools</h2>
-            <p>Tools for administration of the Edgeberry Dashboard</p>
+      <Container style={{textAlign:'left'}}>
+        <br/>
+        <h2><FontAwesomeIcon icon={faUnlockKeyhole}/> Admin tools</h2>
+        <p>Tools for administration of the Edgeberry Dashboard</p>
+        <hr/>
+        <Tabs defaultActiveKey="users" className="mb-3">
+          <Tab eventKey="users" title={<><FontAwesomeIcon icon={faUser} /> Users</>}>
+            <Alert><FontAwesomeIcon icon={faInfoCircle}/> Tools for managing the Dashboard users.</Alert>
+          </Tab>
+          <Tab eventKey="devices" title={<><FontAwesomeIcon icon={faMicrochip}/> Devices</>}>
+            <Alert><FontAwesomeIcon icon={faInfoCircle}/> Tools for managing the devices.</Alert>
+          </Tab>
+        </Tabs>
       </Container>
   );
 }
