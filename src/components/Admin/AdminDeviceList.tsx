867ad097ef26a349064653906ff56f2aa295b293
Tom Santens <tom.santens@gmail.com>
2024-07-01 19:50:13 +0200

    Admin: device list cleanup

--- src/components/Admin/AdminDeviceList.tsx
+++ src/components/Admin/AdminDeviceList.tsx
@@ -3,8 +3,6 @@ import { Card, Col, Container, Row } from "react-bootstrap";
 import { api_things_getThingIndex, api_things_getThingShadow} from "../../api/things";
 import NotificationBox from "../Notification";
 import StatusIndicator from "../StatusIndicator";
-import { direct_identifySystem } from "../../api/directMethods";
-import LoaderOverlay from "../LoadingOverlay";
 import { api_admin_getDeviceList } from "../../api/admin";
 
 const AdminDeviceList = (props:{selected?:string})=>{
@@ -46,31 +44,14 @@ const AdminDeviceListItem = (props:{thing:any, selected:boolean})=>{
     const[ deviceName, setDeviceName ] = useState<string>('');          // the name the user gave to the device
     const[ devicePlatform, setDevicePlatform ] = useState<string>('');  // the platform (e.g. Raspberry Pi 3B+)
 
-    // Overlay
-    const[ hidden, setHidden ] = useState<boolean>(true);
-    const[ text, setText ] = useState<string>('Loading');
-    const[ subText, setSubText ] = useState<string>('Loading');
-
     useEffect(()=>{
         updateAsset();
-        // Poll the device info... bad idea... use sockets...
-        // setInterval(()=>{updateAsset(false)},2000);
     },[]);
-
-    // Set the spinner
-    function setSpinner( visible:boolean, text?:string, subtext?:string ){
-        setHidden(!visible);
-        setText(text?text:'');
-        setSubText(subtext?subtext:'');
-    }
 
     // Update Asset
     async function updateAsset(){
-        setSpinner( true );
         await getThingIndex();
         await getThingShadow();
-        // Spinner is hidden (or kept) by result
-        // from the device shadow system state (!)
     }
 
     // The 'Thing Index' contains information like
@@ -82,8 +63,6 @@ const AdminDeviceListItem = (props:{thing:any, selected:boolean})=>{
         const result = await api_things_getThingIndex(props.thing.thingName);
         if( result.message ){
             // TODO - do something with the error...
-            // although this rarely/never errors. Low priority
-            return setSpinner(true, result.message, 'Retry later...');
         }
         setConnected(result?.connectivity?.connected);
         setDeviceName(result?.attributes?.deviceName);
@@ -96,38 +75,28 @@ const AdminDeviceListItem = (props:{thing:any, selected:boolean})=>{
         const result = await api_things_getThingShadow(props.thing.thingName);
         if( result.message ){
             // TODO - do something with the error...
-            // although this rarely/never errors. Low priority
-            return setSpinner(true, result.message, 'Retry later...');
         }
         // The device hardware platform is also in this info
         setDevicePlatform(result?.state?.reported?.system?.system?.platform);
-
-        // The system state is maybe too deeply nested in the
-        // 'thing shadow', but here it is. If the system is not 'running',
-        // keep the spinner going and report the last known system state
-        if( result?.state?.reported?.system?.system?.state !== 'running'){
-            return setSpinner(true, result?.state?.reported?.system?.system?.state );
-        }
-        return setSpinner(false, result?.state?.reported?.system?.system?.state );
     }
 
     return(
-        <Container className="asset-card-container">
-            <Card className="asset-card">
-                <LoaderOverlay text={text} subtext={subText} spinner hidden={hidden}/>
+        <Container style={{padding:'1px'}}>
+            <Card>
                 <Row>
-                    <Col xs='3' sm='2' md='1'>
-                        <img src={process.env.PUBLIC_URL+'/Edgeberry_rendering.png'} style={{height:'60px'}} onClick={()=>{direct_identifySystem(props.thing.thingName)}} />
+                    <Col xs='3' sm='2' md='1' style={{padding:'15px'}}>
+                        <img src={process.env.PUBLIC_URL+'/Edgeberry_rendering.png'} width="100%" />
                     </Col>
                     <Col>
-                        <div className="asset-card-body" style={{height:'100%', padding:'5px'}}>
-                        <Card.Title className="asset-card-title">
-                            <StatusIndicator noText message={connected?"Online":"Offline"} type={connected?"success":"danger"} />&nbsp;
-                            {props.thing.thingName}
-                        </Card.Title>
-                        <Card.Text className="asset-card-text">
-                            {devicePlatform?devicePlatform:props?.thing?.thingTypeName}
-                        </Card.Text>
+                        <div style={{height:'100%', padding:'5px'}}>
+                            <div style={{fontWeight:'bold', fontSize:'1.2rem'}}>
+                                <StatusIndicator noText message={connected?"Online":"Offline"} type={connected?"success":"danger"} />&nbsp;
+                                {props.thing.thingName}
+                            </div>
+                            <div style={{color:'var(--edgeberry_dark_gray)'}}>
+                                {devicePlatform?devicePlatform:"No platform"}<br/>
+                                {deviceName?deviceName:"No device name"}
+                            </div>
                         </div>
                     </Col>
                 </Row>
